2e467c44748829efae25e89bc27de4511548ee55
Joerakesh <rakeshjoe52@gmail.com>
2025-01-02 19:17:16 +0530

    commit

--- js/footer.js
+++ js/footer.js
@@ -2,9 +2,9 @@ class SpecialFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
          <footer class="footer-48201" id="footer">
-      <div class="container" >
+      <div class="container">
         <div class="row">
-          <div class="col-md-4 pr-md-5" >
+          <div class="col-md-4 pr-md-5">
             <a href="#" class="footer-site-logo d-block mb-4"
               >Publisher Information</a
             >
@@ -25,11 +25,12 @@ class SpecialFooter extends HTMLElement {
             <h3 style="color: white">Editor in chief</h3>
             <p>Prasanth Arokia Samy. D</p>
             <h3 style="color: white">Contact Us</h3>
-            <p style="text-transform: lowercase">
-              +91 9994494858 <br />
-              sjcengzine@mail.sjctni.edu
-            </p>
-
+            <p style="text-transform: lowercase; margin: 0">+91 9994494858</p>
+            <a
+              href="mailto:sjcengzine@mail.sjctni.edu"
+              style="text-transform: none; margin: 0"
+              >sjcengzine@mail.sjctni.edu</a
+            >
             <hr />
           </div>
           <div class="col-md">
@@ -43,6 +44,28 @@ class SpecialFooter extends HTMLElement {
               <li><a href="/components/submissions.html">Submissions</a></li>
             </ul>
             <hr />
+            <ul class="list-unstyled nav-links">
+              <a href="#" class="footer-site-logo d-block mb-4"
+                >Website designed and developed by</a
+              >
+              <li>
+                <a href="#" target="_blank">Ashore Nikshan J </a
+                ><a href="" target="_blank"
+                  ><i class="fa-brands fa-linkedin" style="font-size: 15px"></i
+                ></a>
+              </li>
+              <li>
+                <a
+                  href="https://joerakesh-portfolio.netlify.app/"
+                  target="_blank"
+                  >Joe Rakesh A </a
+                ><a
+                  href="https://www.linkedin.com/in/joe-rakesh-27b082286/"
+                  target="_blank"
+                  ><i class="fa-brands fa-linkedin" style="font-size: 15px"></i
+                ></a>
+              </li>
+            </ul>
           </div>
           <div class="col-md">
             <ul class="list-unstyled nav-links">
@@ -86,9 +109,8 @@ class SpecialFooter extends HTMLElement {
                     >&copy; www.sjctni.edu, All Rights Reserved.</small
                   >
                 </a>
-                
               </p>
-              <!--  <p><a href="">Developed by Joe Rakesh</a></p>--> 
+              <!--  <p><a href="">Developed by Joe Rakesh</a></p>-->
             </div>
           </div>
         </div>
